30ef3502424e53a3918869bc6585c0f1291d2840
IkeyBenz <ikey.benz@gmail.com>
2019-10-24 14:53:40 -0700

    implemented bubble sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -11,13 +11,16 @@ def is_sorted(items):
     return True
 
 
-def bubble_sort(items):
+def bubble_sort(items, j=0):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    Running Time: O(n^2), itterates over n items n times
+    Memory usage: O(1) not storing any data from items"""
+    for i in range(len(items)-1-j):
+        if items[i] > items[i+1]:
+            items[i], items[i+1] = items[i+1], items[i]
+    if j < len(items):
+        bubble_sort(items, j+1)
 
 
 def selection_sort(items):
