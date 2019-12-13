9ef154057a23a8199be331691167ebf910791976
IkeyBenz <ikey.benz@gmail.com>
2019-12-13 15:33:41 -0800

    implemented bubbles

--- Code/binaryheap.py
+++ Code/binaryheap.py
@@ -93,10 +93,12 @@ class BinaryMinHeap(object):
         # Get the parent's index and value
         parent_index = self._parent_index(index)
         parent_item = self.items[parent_index]
-        # TODO: Swap this item with parent item if values are out of order
-        # ...
-        # TODO: Recursively bubble up again if necessary
-        # ...
+        # Swap this item with parent item if values are out of order
+        if item < parent_item:
+            # Swap this item with parent item
+            self.items_swap(index, parent_index)
+            # Recursively bubble up again if necessary
+            self._bubble_up(parent_index)
 
     def _bubble_down(self, index):
         """Ensure the heap ordering property is true below the given index,
@@ -113,14 +115,15 @@ class BinaryMinHeap(object):
             return  # This index is a leaf node (does not have any children)
         # Get the item's value
         item = self.items[index]
-        # TODO: Determine which child item to compare this node's item to
-        child_index = 0
-        # ...
-        # TODO: Swap this item with a child item if values are out of order
-        child_item = self.items[child_index]
-        # ...
-        # TODO: Recursively bubble down again if necessary
-        # ...
+        largest = index
+        if left_index <= self._last_index() and self.items[left_index] < item:
+            largest = left_index
+        if right_index <= self._last_index() and self.items[right_index] < self.items[largest]:
+            largest = right_index
+        if largest != index:
+            self.items_swap(index, largest)
+            if not self._is_leaf(largest):
+                self._bubble_down(largest)
 
     def _last_index(self):
         """Return the last valid index in the underlying array of items."""
